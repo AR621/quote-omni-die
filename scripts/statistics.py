5ac424d127ae7aeeec167b54df83382105a772b8
AR621 <60547627+AR621@users.noreply.github.com>
2022-05-12 09:24:00 +0200

    Update statistics.py

--- scripts/statistics.py
+++ scripts/statistics.py
@@ -37,16 +37,23 @@ authors = [quote_json.get('author') for quote_json in quote_jsons]
 
 # %% unique appearances
 c = collections.Counter(authors) # we count number of appearances of each author
-
+c=sorted(c.items(), key=lambda x:x[1]) # sort the dictionary so when displayed on pie chart
+# it looks well ordered
+c = dict(c)
 # %% pie plot
+
 labels = c.keys()
 values = c.values()
-explode = [0.1]*len(labels)
+explode = [0.6/appearances for appearances in values] # the bigger the slice the closer it will be to the center
 
-f=plt.Figure()
-ax=plt.pie(
+f=plt.Figure(figsize=(48,48))
+patches, labels, pct_texts = plt.pie(
     values,
     rotatelabels=True,
     autopct='%0.1f%%',
     explode=explode,
+    startangle=-180,
     labels=labels)
+# rotate the the percentage label same way as author label                                    
+for label, pct_text in zip(labels, pct_texts):
+    pct_text.set_rotation(label.get_rotation())
