fd7ab007c44b6167719f36d45904d4d3618141fa
AR621 <60547627+AR621@users.noreply.github.com>
2022-09-01 09:34:20 +0200

    154->160 + quoteBringer.py big update
    Improved quotebringer.py to allow for much more flexible filtering as well as for nicer integrations with terminal for overall user experience improvment

--- scripts/quote_bringer.py
+++ scripts/quote_bringer.py
@@ -4,9 +4,11 @@
 @author: AR621
 This script finds quotes based on search criteria
 """
-import json
 import os
 import sys
+import argparse
+from itertools import compress
+from pprint import pprint  # for that nice list printing :)
 
 # own imports
 # paths (so the script works when invoked out of its own dir)
@@ -22,15 +24,45 @@ authors = loader.load_quotes(get_quotes=False, get_authors=True)
 quotes = loader.load_quotes(get_quotes=True, get_authors=False)
 
 
-# %% Function for displaying specific author quotes from the repo
-def search_quote_by_author(search_criteria):
-    for index in range(len(authors)):
+# %% Function for displaying specific quotes from the repo by provide search criteria
+def search_string_list_by_criteria(search_criteria, unfiltered_list):
+    filtered_index_list = [False for i in range(len(unfiltered_list))]
+    for index in range(len(unfiltered_list)):
         # add condition for the search here
-        if authors[index] == search_criteria:
-            print("quote id: %s" % str(index))
-            print(quotes[index])
-            print(authors[index])
-            print()
+        if search_criteria.lower() in unfiltered_list[index].lower():  # <- .lower() for case "insensiveness"
+            filtered_index_list[index] = True
+    return filtered_index_list
 
 
-search_quote_by_author('Helen Keller')
+def preetyprint_filtered_quotes(authors_list, quotes_list):
+    if not authors_list:
+        pprint("QuoteBringer.py: Found no quotes matching the search criteria")
+    else:
+        pprint("QuoteBringer.py: Found the following quotes matching the criteria")
+        for author, quote in zip(authors_list, quotes_list):
+            pprint(author)
+            pprint(quote)
+
+# print("quote id: %s" % str(index))
+# print(quotes[index])
+# print(authors[index])
+# print()  # and a lazy newline at the end
+
+
+# search_quote_by_author('Helen Keller')
+
+parser = argparse.ArgumentParser(description="quote_bringer.py help",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-a", "--author", default='', help="filter output by specific author")
+parser.add_argument("-q", "--quote", default='', help="filter output by specific substring in the quotes")
+args = vars(parser.parse_args())
+
+# if args[author] != None:
+
+search_filter = search_string_list_by_criteria(args['author'], authors)
+search_filter = search_string_list_by_criteria(args['quote'], list(compress(quotes, search_filter)))
+
+filtered_authors = (list(compress(authors, search_filter)))
+filtered_quotes = (list(compress(quotes, search_filter)))
+
+preetyprint_filtered_quotes(filtered_authors, filtered_quotes)
